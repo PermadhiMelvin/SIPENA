d02c4b4551ea8a3a6eae04d517c0327cd134015a
Tsaqif06 <tsaqifcakep87@gmail.com>
2023-08-08 19:36:07 +0700

    Update table ruangs

--- database/migrations/2023_08_08_095652_create_ruangs_table.php
+++ database/migrations/2023_08_08_095652_create_ruangs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('ruangs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->string('');
+            $table->boolean('is_dipakai')->default(false);
             $table->timestamps();
         });
     }
